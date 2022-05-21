145b7d72f78e73965c2cf12c38e6689984e495c4
Gustavo Leme <gustavo_leme05@hotmail.com>
2022-05-21 15:12:17 -0300

    Update - menu e texto
    Implementar uma função à parte para melhor otimização;
    Implementar uma função para que o texto decorra carácter por carácter;
    Criar variáveis globais.

--- Jogo/Tech Sync/src/Main.java
+++ Jogo/Tech Sync/src/Main.java
@@ -2,18 +2,42 @@
 import java.util.*;
 
 public class Main {
+    //Declaração de variáveis
+    static String optionMenu, optionGame, history;
+    static boolean menu = true, jogar = false;
+
+    //Instanciamento de objetos
+    static Scanner input = new Scanner(System.in);
+
     //Função principal, onde o programa se iniciará
-    public static void main(String[] args) {
-        //Declaração de variáveis
-        String optionMenu, optionGame;
+    public static void main(String[] args) throws InterruptedException {
+        //Inicialização do menu
+        Menu();
 
-        //Instanciamento de objetos
-        Scanner input = new Scanner(System.in);
+        while(jogar){
+            //Introdução
+            history = "\n\n~~TECH SYNC~~";
+            texting(history);
 
+            //Primeira fase
+            history = "\n\nAo acordar, deparei-me com um lugar estranho. Tudo parecia antigo e ultrapassado e apesar de parecer acabado,\nnão conseguia sentir dor nenhuma... Na verdade, não sentia nada, não conseguia sequer lembrar de onde vim,\ncomo apareci aqui e nem para onde eu tinha que ir. A única motivação que eu tinha parecia martelar por dentro\nda minha memória, como coordenadas que me levariam para algum lugar importante e, por instinto, decidi que\nseguiria o caminho designado.\n";
+            texting(history);
+            //Chamando o menu para que seja verificado se deseja jogar novamente ou encerrar o programa
+            Menu();
+        }
+        
+        
+        history = "\nObrigado por jogar :D\n";
+        
+    }
+
+    public static boolean Menu() throws InterruptedException {
         //Inicio do jogo - MENU
-        System.out.println("Bem vindo ao Tech Sync\nJogo RPG Textual hambientado em um mundo futurista\n\n*INSTRUÇÃO para uso geral, ao escolher a opção desejada ignore qualquer acentuação e letra maiúscula,\npor exemplo, em 'Instruções' escreva 'intrucoes'!*");
+        history ="Bem vindo ao Tech Sync\nJogo RPG Textual hambientado em um mundo futurista\n\n*INSTRUÇÃO para uso geral, ao escolher a opção desejada ignore qualquer acentuação e letra maiúscula,\npor exemplo, em 'Instruções' escreva 'intrucoes'!*";
+        texting(history);
         do{
-            System.out.println("\n    MENU\n---Jogar---\n--Tutorial--\n--Créditos--\n---Sair---");
+            history ="\n    MENU\n---Jogar---\n--Tutorial--\n--Créditos--\n---Sair---\n";
+            texting(history);
             System.out.print("Opção: ");
             optionMenu = input.next();
 
@@ -21,44 +45,54 @@ public class Main {
                 //Inicio do jogo - HISTÓRIA
                 case "Jogar":
                 case "jogar":
+                    jogar = true;
+                    menu = false;
                     
                     break;
                 //Tutorial de como o jogador deve seguir no jogo
                 case "Tutorial":
                 case "tutorial":
-                    System.out.println("\nVocê começará como (LORE),\nao passo que você/seu personagem percorre pela história serão mostradas opções de ações possíveis,\né preferível que ao escolher um opção ela seja digitada ignorando sua acentuação e escrita totalmente em letras minúsculas,\n(MECANICA DE JOGO),\n(RECOMPENSA FINAL).\n\nEsperamos mesmo que se divirtão e aprendam jogando este jogo feito com o máximo de cuidado e atenção que essas poucas pessoas puderam tudo de si,\nAgradecemos e let's bora!");
+                    history = "\nVocê começará como (LORE),\nao passo que você/seu personagem percorre pela história serão mostradas opções de ações possíveis,\né preferível que ao escolher um opção ela seja digitada ignorando sua acentuação e escrita totalmente em letras minúsculas,\n(MECANICA DE JOGO),\n(RECOMPENSA FINAL).\n\nEsperamos mesmo que se divirtão e aprendam jogando este jogo feito com o máximo de cuidado e atenção que essas poucas pessoas puderam tudo de si,\nAgradecemos e let's bora!\n";
                     break;
                 //Responsáveis pelo projeto
                 case "Creditos":
                 case "creditos":
-                    System.out.println("\n~Le pika/DEV: Gusta;p\n~Le carregado/escritor: Lucky*_*");
+                    history = "\n~Le pika/DEV: Gusta;p\n~Le carregado/escritor: Lucky*_*\n";
                     do{
-                        System.out.print("\nDeseja voltar ao Menu? ");
+                        history = "\nDeseja voltar ao Menu? ";
                         optionMenu = input.next();
                         if(optionMenu.equalsIgnoreCase("Sim")){
-                            optionMenu = "Menu";
+                            menu = true;
                             break;
                         }else if(optionMenu.equalsIgnoreCase("Nao")){
-                            optionMenu = "";
+                            menu = false;
                             break;
                         }else{
-                            System.out.println("Digite somente Sim ou Não");
+                            history = "Digite somente Sim ou Não");
                         }
                     }while(!optionMenu.equalsIgnoreCase("Sim") || !optionMenu.equalsIgnoreCase("Nao"));
                     break;
                 case "Sair":
                 case "sair":
-                    System.out.println("\n\nEsperamos que tenha se divertido!");
-                    optionMenu = "";
+                    history = "\n\nEsperamos que tenha se divertido!");
+                    jogar = false;
+                    menu = false;
                     break;
                 default:
-                    System.out.println("\nComando inválido ou digitado de maneira errada, como intruido pode ignorar qualquer acentuação e letras maiusculas,\npor exemplo, ao invés de escrever 'Instruções' escreva 'intrucoes'!");
+                    history = "\nComando inválido ou digitado de maneira errada, como intruido pode ignorar qualquer acentuação e letras maiusculas,\npor exemplo, ao invés de escrever 'Instruções' escreva 'intrucoes'!\n";
             }
-        }while(optionMenu.equalsIgnoreCase("Menu"));
-        
-        System.out.println("\nObrigado por jogar :D");
+        }while(menu);
+        return jogar;
+    }
 
-        //Finalização de objetos
-        input.close();
+    public static void Player(){
+
+    }
+
+    public static void texting(String history) throws InterruptedException{
+        for(int i = 0; i < history.length(); i++){
+            Thread.sleep(80);
+            System.out.print(history.charAt(i));
+        }
     }
 }
