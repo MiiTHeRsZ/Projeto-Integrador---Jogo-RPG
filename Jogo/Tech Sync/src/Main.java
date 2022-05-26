a5698f35dec95c622e11188ec572f700049a09a6
Lucas Paes <105465128+XLuckyJPX@users.noreply.github.com>
2022-05-26 12:31:05 -0300

    Texto estilizado Início
    input de texto estilizado para

--- Jogo/Tech Sync/src/Main.java
+++ Jogo/Tech Sync/src/Main.java
@@ -35,10 +35,13 @@ public class Main {
     //Menu
     public static boolean Menu() throws InterruptedException {
         //Inicio do jogo
+        history = "    ███        ▄████████  ▄████████    ▄█    █▄            ▄████████ ▄██   ▄   ███▄▄▄▄    ▄████████ \n▀█████████▄   ███    ███ ███    ███   ███    ███          ███    ███ ███   ██▄ ███▀▀▀██▄ ███    ███ \n   ▀███▀▀██   ███    █▀  ███    █▀    ███    ███          ███    █▀  ███▄▄▄███ ███   ███ ███    █▀  \n    ███   ▀  ▄███▄▄▄     ███         ▄███▄▄▄▄███▄▄        ███        ▀▀▀▀▀▀███ ███   ███ ███        \n    ███     ▀▀███▀▀▀     ███        ▀▀███▀▀▀▀███▀       ▀███████████ ▄██   ███ ███   ███ ███        \n    ███       ███    █▄  ███    █▄    ███    ███                 ███ ███   ███ ███   ███ ███    █▄  \n    ███       ███    ███ ███    ███   ███    ███           ▄█    ███ ███   ███ ███   ███ ███    ███ \n   ▄████▀     ██████████ ████████▀    ███    █▀          ▄████████▀   ▀█████▀   ▀█   █▀  ████████▀  \n                                                                                                    \n\n";
+        texting(history, 1);
+
         history = "Bem vindo ao Tech Sync\nJogo RPG Textual ambientado em um mundo futurista\n\n*INSTRUÇÃO para uso geral, ao escolher a opção desejada ignore qualquer acentuação, por exemplo,\nem 'Instruções' escreva 'intrucoes'!*\n";
         texting(history, 20);
         do{
-            history = "\n    MENU\n--- Jogar---\n--Tutorial--\n--Créditos--\n----Sair----\n\nOpção: ";
+            history = "\n    MENU\n----Jogar---\n--Tutorial--\n--Créditos--\n----Sair----\n\nOpção: ";
             texting(history, 20);
             optionMenu = input.next();
 
